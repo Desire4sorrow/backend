0e211eda29a87bed78920e8daf44ff5ff5de2ba3
Ruslan <56004655+Desire4sorrow@users.noreply.github.com>
2020-03-24 18:46:54 +0300

    Update Program.cs

--- 3. Remove_Duplicates/remove_duplicates/Program.cs
+++ 3. Remove_Duplicates/remove_duplicates/Program.cs
@@ -1,38 +1,26 @@
 using System;
+
 namespace RemoveDuplicates
 {
-
     class Program
-
     {
-
         private static void Main(string[] args)
 
         {
-
             if (args.Length != 1)
-
             {
-
                 Console.WriteLine("Incorrect number of arguments!");
 
                 Console.WriteLine("Usage: remove_duplicates.exe <input string>");
 				
                 Environment.Exit(1);
-
             }
-			else
-
+	    else
             {
-
                 string st = args[0];
 
                 Console.WriteLine(st.Distinct().ToArray());
-
             }
-
         }
-
     }
-
 }
