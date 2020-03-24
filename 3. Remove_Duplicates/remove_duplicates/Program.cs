80287351aece4992140687378f96beb0e2a6657e
Ruslan <56004655+Desire4sorrow@users.noreply.github.com>
2020-03-24 18:44:33 +0300

    Add files via upload

---
+++ 3. Remove_Duplicates/remove_duplicates/Program.cs
@@ -0,0 +1,38 @@
+using System;
+namespace RemoveDuplicates
+{
+
+    class Program
+
+    {
+
+        private static void Main(string[] args)
+
+        {
+
+            if (args.Length != 1)
+
+            {
+
+                Console.WriteLine("Incorrect number of arguments!");
+
+                Console.WriteLine("Usage: remove_duplicates.exe <input string>");
+				
+                Environment.Exit(1);
+
+            }
+			else
+
+            {
+
+                string st = args[0];
+
+                Console.WriteLine(st.Distinct().ToArray());
+
+            }
+
+        }
+
+    }
+
+}
